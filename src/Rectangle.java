052810b698e448c93193a2956fe612ee858ed800
JackGriebel <Professorjack88@gmail.com>
2017-08-03 23:09:51 -0400

    Add files via upload

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,7 +1,6 @@
 import org.w3c.dom.css.Rect;
 
 import java.awt.*;
-
 public class Rectangle {
     Vector position;
     Vector speed;
@@ -11,11 +10,9 @@ public class Rectangle {
     int rectSpeed;
     boolean isBullet;
     int rectType;
-    int age;
-    boolean outOfBounds;
 
 
-    public Rectangle(Vector position, Vector speed, Vector size, Color color, boolean movesVert, int rectSpeed, boolean isBullet, int rectType, int age, boolean outOfBounds) {
+    public Rectangle(Vector position, Vector speed, Vector size, Color color, boolean movesVert, int rectSpeed, boolean isBullet, int rectType) {
         this.position = position;
         this.speed = speed;
         this.size = size;
@@ -24,33 +21,30 @@ public class Rectangle {
         this.rectSpeed = rectSpeed;
         this.isBullet = isBullet;
         this.rectType = rectType;
-        this.age = age;
-        this.outOfBounds = outOfBounds;
     }
-
     public void draw(Graphics2D g, Rectangle rect) {
         g.setColor(color);
-        g.drawRect((int) rect.position.x, (int) rect.position.y, (int) rect.size.x, (int) rect.size.y);
-        if (!rect.isBullet) {
-            if (rect.rectType == 0) {
+        g.drawRect((int)rect.position.x, (int)rect.position.y, (int)rect.size.x, (int)rect.size.y);
+        if(!rect.isBullet) {
+            if(rect.rectType == 0) {
                 int xPoly[] = {(int) rect.position.x, (int) (rect.position.x + (rect.size.x / 2)), (int) (rect.position.x + (rect.size.x)), (int) (rect.position.x + (rect.size.x / 2))};
                 int yPoly[] = {(int) (rect.position.y + (rect.size.x / 2)), (int) (rect.position.y + (rect.size.x)), (int) (rect.position.y + (rect.size.x / 2)), (int) (rect.position.y)};
                 g.drawPolygon(xPoly, yPoly, xPoly.length);
-            } else if (rect.rectType == 1) {
-                g.drawOval((int) rect.position.x, (int) rect.position.y, (int) rect.size.x, (int) rect.size.y);
-            } else if (rect.rectType == 2) {
+            } else if(rect.rectType == 1) {
+                g.drawOval((int)rect.position.x,(int) rect.position.y, (int)rect.size.x, (int)rect.size.y);
+            } else if(rect.rectType == 2) {
                 int rectSmallFactor = 20;
-                g.drawRect((int) (rect.position.x + ((rect.size.x - rectSmallFactor * 1.5) / 2)), (int) (rect.position.y + ((rect.size.x - rectSmallFactor * 1.5) / 2)), (int) (rect.size.x - rectSmallFactor), (int) (rect.size.x - rectSmallFactor));
+                g.drawRect((int)(rect.position.x + ((rect.size.x - rectSmallFactor * 1.5) / 2)), (int)(rect.position.y + ((rect.size.x - rectSmallFactor * 1.5) / 2)), (int)(rect.size.x - rectSmallFactor), (int)(rect.size.x - rectSmallFactor));
             } else if (rect.rectType == 3) {
-                int xPoly[] = {(int) rect.position.x, (int) (rect.position.x + rect.size.x), (int) (rect.position.x + rect.size.x / 2)};
-                int yPoly[] = {(int) (rect.position.y + rect.size.y), (int) (rect.position.y + rect.size.y), (int) (rect.position.y)};
+                int xPoly[] = {(int) rect.position.x, (int) (rect.position.x + rect.size.x), (int)(rect.position.x + rect.size.x / 2)};
+                int yPoly[] = {(int) (rect.position.y + rect.size.y), (int) (rect.position.y + rect.size.y),(int)(rect.position.y) };
                 g.drawPolygon(xPoly, yPoly, xPoly.length);
-            } else if (rect.rectType == 4) {
+            } else if(rect.rectType == 4) {
                 int smallRectSize = 20;
-                g.drawRect((int) rect.position.x, (int) rect.position.y, smallRectSize, smallRectSize);
-                g.drawRect((int) (rect.position.x), (int) (rect.position.y + rect.size.x - smallRectSize), smallRectSize, smallRectSize);
-                g.drawRect((int) (rect.position.x + rect.size.x - smallRectSize), (int) rect.position.y, smallRectSize, smallRectSize);
-                g.drawRect((int) (rect.position.x + rect.size.x - smallRectSize), (int) (rect.position.y + rect.size.x - smallRectSize), smallRectSize, smallRectSize);
+                g.drawRect((int)rect.position.x, (int)rect.position.y, smallRectSize, smallRectSize);
+                g.drawRect((int)(rect.position.x), (int)(rect.position.y + rect.size.x - smallRectSize), smallRectSize, smallRectSize);
+                g.drawRect((int)(rect.position.x + rect.size.x - smallRectSize), (int)rect.position.y, smallRectSize, smallRectSize);
+                g.drawRect((int)(rect.position.x + rect.size.x - smallRectSize), (int)(rect.position.y + rect.size.x - smallRectSize), smallRectSize, smallRectSize);
             }
         }
     }
@@ -62,33 +56,26 @@ public class Rectangle {
      * @param movesVert - weather it moves verticle or horizontal
      */
     boolean rectangleAlive = true;
-
     public void update(int dt, Rectangle rectangle, Graphics2D g, boolean movesVert, int obstSize) {
-
-        //moves bullets free of collision
-        if (rectangle.isBullet) {
-            if (!rectangle.outOfBounds) {
-                rectangle.position.add(rectangle.speed);
-                draw(g, rectangle);
-            }
+        if(rectangle.isBullet) {
+            rectangle.position.add(rectangle.speed);
+            draw(g, rectangle);
         } else {
-            //checks void collision <--- useful to future me
-            if (!Game.checkCollision(new Vector(820, 455), rectangle, 50, 80)) {
-                rectangleAlive = false;
-            }
-            if (rectangleAlive) {
-                draw(g, rectangle);
-                speed = Vector.sub(Game.characterPosition, rectangle.position);
-                speed.setMag(rectangle.rectSpeed);
-                rectangle.position.add(speed);
-                rectangle.size = new Vector(obstSize, obstSize);
-            }
+        if(!Game.checkCollision(new Vector(493, 360), rectangle, 50, 80)) {
+            rectangleAlive = false;
         }
-    }
+        if(rectangleAlive) {
+            draw(g, rectangle);
+            speed = Vector.sub(Game.characterPosition, rectangle.position);
+            speed.setMag(rectangle.rectSpeed);
+            rectangle.position.add(speed);
+            rectangle.size = new Vector(obstSize, obstSize);
+        } } }
 
 
     public boolean newRectangleNeeded(Vector characterPosition, Rectangle rectangle, int OBSTSIZE, int characterSize) {
-        if (!Game.checkCollision(characterPosition, rectangle, OBSTSIZE, characterSize)) {
+        if (!Game.checkCollision(characterPosition, rectangle, OBSTSIZE, characterSize))
+        {
             System.out.println("Collision detected");
             return true;
         } else {
@@ -98,19 +85,22 @@ public class Rectangle {
     }
 
     public void makeNewRects(boolean newRectangleNeeded, Rectangle rectangle, int WIDTH) {
-        if (newRectangleNeeded) {
-            if (!rectangle.movesVert) {
+        if(newRectangleNeeded) {
+            if(!rectangle.movesVert) {
                 rectangle.position.x = 88;
-                rectangle.position.y = (int) (Math.random() * 550);
+                rectangle.position.y =  (int)(Math.random() * 550);
             } else {
-                rectangle.position.x = (int) (Math.random() * (WIDTH - 50) + 88);
+                rectangle.position.x = (int) (Math.random() * (WIDTH- 50) + 88);
                 rectangle.position.y = 0;
                 System.out.println("Vert Rect Moved");
             }
         }
 
 
+
     }
 
 
-}
+
+
+        }
